962361f93a1bc3daa65aea66e4e209da0c1c6b31
Victoria Barkfeld <victoriacbarkfeld@gmail.com>
2021-06-28 15:30:30 -0300

    Adicionando classes

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,4 +1,13 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
+    var nome: String = "Victoria"
+    var cpf: String = "123.456.678-89"
+}
+
+fun main() {
+    val Victoria = Pessoa()
+
+    println(Victoria.nome)
+    println(Victoria.cpf)
 }
